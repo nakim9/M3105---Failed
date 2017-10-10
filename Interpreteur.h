a2cd69f9499d8f1a7fec8cbf6a84842d7db499f2
epalley <epalley@pc-dg-027-03.iut2.upmf-grenoble.fr>
2017-10-10 14:55:49 +0200

    Premier test

--- Interpreteur.h
+++ Interpreteur.h
@@ -35,6 +35,7 @@ private:
                            //   <opBinaire> ::= + | - | *  | / | < | > | <= | >= | == | != | et | ou
     Noeud*  instSi();      //      <instSi> ::= si ( <expression> ) <seqInst> finsi
     Noeud*  instTantQue(); //   <instTantQue> ::= tantque ( <expression> ) <seqInst> fintantque
+    Noeud*  instRepeter(); //   <instRepeter> ::= repeter <seqInst> jusqua( <expression> )
 
     // outils pour simplifier l'analyse syntaxique
     void tester (const string & symboleAttendu) const throw (SyntaxeException);   // Si symbole courant != symboleAttendu, on lève une exception
